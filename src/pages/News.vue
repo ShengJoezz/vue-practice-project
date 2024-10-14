9f9a020810d96971de011d9ac999d6362ad45386
joe <847304926@qq.com>
2024-10-14 23:07:43 +0800

    learning Query and Vue-Children-Router

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -14,8 +14,15 @@ const news_list  = reactive([
     {name : 'fire',data:20240917},
     {name : 'water',data:20240916}
 
-])
-</script>
-
-<style scoped>
+  // 格式化日期函数
+  function formatDate(date: number) {
+    const str = date.toString();
+    const year = str.slice(0, 4);
+    const month = str.slice(4, 6);
+    const day = str.slice(6, 8);
+    return `${year}-${month}-${day}`;
+  }
+  </script>
+  
+  <style scoped>
 </style>
